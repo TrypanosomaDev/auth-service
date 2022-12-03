07c28eff251fc7c29aeb8cb8c2a69614682febbf
BerkeEvrensevdi <berke.evrensevdi@hotmail.com>
2022-12-03 14:10:25 +0300

    added null check to registration request body

--- src/main/java/com/example/authservice/registration/RegistrationController.java
+++ src/main/java/com/example/authservice/registration/RegistrationController.java
@@ -4,13 +4,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping(path = "api/v1/registration")
 @RequiredArgsConstructor
 public class RegistrationController {
     private final RegistrationService registrationService;
     @PostMapping
-    public String register(@RequestBody RegistrationRequest request) {
+    public String register(@Valid @RequestBody RegistrationRequest request) {
         return registrationService.register(request);
     }
 
--- src/main/java/com/example/authservice/registration/RegistrationRequest.java
+++ src/main/java/com/example/authservice/registration/RegistrationRequest.java
@@ -6,14 +6,21 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @AllArgsConstructor
 @EqualsAndHashCode
 @ToString
 public class RegistrationRequest {
+    @NotNull
     private final String firstName;
+    @NotNull
     private final String lastName;
+    @NotNull
     private final String email;
+    @NotNull
     private final String password;
+    @NotNull
     private final AppUserRole userRole;
 }
